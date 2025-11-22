18d5fa33da50422a86d3e7ef6b63f4add5fdf313
Sanga_Bhattacharjee <sangabhattacharjee544@gmail.com>
2025-11-23 01:06:00 +0530

    Resolved merge conflicts

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,11 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-<<<<<<< HEAD
       <p>⚡ Built by DevHive 2025 • Aurora Project</p>
-=======
-      <p>⚡ Built for Hackaura 2025 • Aurora Project</p>
->>>>>>> 85ffe0da6c7618068a6517ca4fb223425ada78ee
     </footer>
   );
 }
